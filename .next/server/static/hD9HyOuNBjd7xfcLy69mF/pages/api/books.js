782ad81f32c274fa2694cccc36e8c8092a6af793
Dhaneshen <dan.moonian.yahoo.com>
2020-07-01 01:00:47 -0600

    Working with MongoDB, working

--- .next/server/static/hD9HyOuNBjd7xfcLy69mF/pages/api/books.js
+++ .next/server/static/hD9HyOuNBjd7xfcLy69mF/pages/api/books.js
@@ -1,7 +1,7 @@
 module.exports =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
-/******/ 	var installedModules = {};
+/******/ 	var installedModules = require('../../../../ssr-module-cache.js');
 /******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
@@ -88,23 +88,124 @@ module.exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "KqAr");
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ "KqAr":
+/***/ "/XmJ":
+/***/ (function(module, exports, __webpack_require__) {
+
+const mongoose = __webpack_require__("FiKB");
+
+const BookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Please add a title'],
+    unique: true,
+    trim: true,
+    maxlength: [40, 'Title cannot be more than 40 characters']
+  }
+});
+module.exports = mongoose.models.Book || mongoose.model('Book', BookSchema);
+
+/***/ }),
+
+/***/ 2:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("j2p9");
+
+
+/***/ }),
+
+/***/ "FiKB":
+/***/ (function(module, exports) {
+
+module.exports = require("mongoose");
+
+/***/ }),
+
+/***/ "RuLO":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var mongoose__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("FiKB");
+/* harmony import */ var mongoose__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(mongoose__WEBPACK_IMPORTED_MODULE_0__);
+
+const connection = {};
+
+async function dbConnect() {
+  if (connection.isConnected) {
+    return;
+  }
+
+  const db = await mongoose__WEBPACK_IMPORTED_MODULE_0___default.a.connect("mongodb+srv://profiles:MongoPass@cluster0-f8o3q.gcp.mongodb.net/profiles?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  connection.isConnected = db.connections[0].readyState;
+  console.log(connection.isConnected);
+}
+
+/* harmony default export */ __webpack_exports__["a"] = (dbConnect);
+
+/***/ }),
+
+/***/ "j2p9":
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _utils_dbConnect__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("RuLO");
+/* harmony import */ var _modals_book__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("/XmJ");
+/* harmony import */ var _modals_book__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_modals_book__WEBPACK_IMPORTED_MODULE_1__);
 
-    
 
-    /* harmony default export */ __webpack_exports__["default"] = (function (ctx) {
-      return Promise.all([])
-    });
-  
+Object(_utils_dbConnect__WEBPACK_IMPORTED_MODULE_0__[/* default */ "a"])();
+/* harmony default export */ __webpack_exports__["default"] = (async (req, res) => {
+  const {
+    method
+  } = req;
+
+  switch (method) {
+    case 'GET':
+      try {
+        const books = await _modals_book__WEBPACK_IMPORTED_MODULE_1___default.a.find({});
+        res.status(200).json({
+          success: true,
+          data: books
+        });
+      } catch (error) {
+        res.status(400).json({
+          success: false
+        });
+      }
+
+      break;
+
+    case 'POST':
+      try {
+        const book = await _modals_book__WEBPACK_IMPORTED_MODULE_1___default.a.create(req.body);
+        res.status(201).json({
+          success: true,
+          data: book
+        });
+      } catch (error) {
+        res.status(400).json({
+          success: false
+        });
+      }
+
+      break;
+
+    default:
+      res.status(400).json({
+        success: false
+      });
+      break;
+  }
+});
 
 /***/ })
 
