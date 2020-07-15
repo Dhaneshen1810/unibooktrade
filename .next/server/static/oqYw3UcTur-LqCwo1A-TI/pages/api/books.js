243319594018864ac435250659752188d4c4fcb4
Dhaneshen <dan.moonian.yahoo.com>
2020-07-14 23:22:56 -0600

    map undefined bug

---
+++ .next/server/static/oqYw3UcTur-LqCwo1A-TI/pages/api/books.js
@@ -0,0 +1,417 @@
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = require('../../../../ssr-module-cache.js');
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete installedModules[moduleId];
+/******/ 		}
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "/XmJ":
+/***/ (function(module, exports, __webpack_require__) {
+
+const mongoose = __webpack_require__("FiKB");
+
+const BookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Please add a title'],
+    trim: true,
+    maxlength: [40, 'Title cannot be more than 40 characters']
+  },
+  author: {
+    type: String,
+    required: true,
+    maxlength: [40, 'author cannot be more than 40 characters']
+  },
+  ownerID: {
+    type: Number
+  },
+  ownerName: {
+    type: String
+  }
+});
+module.exports = mongoose.models.Book || mongoose.model('Book', BookSchema);
+
+/***/ }),
+
+/***/ 2:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("j2p9");
+
+
+/***/ }),
+
+/***/ "FiKB":
+/***/ (function(module, exports) {
+
+module.exports = require("mongoose");
+
+/***/ }),
+
+/***/ "RuLO":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var mongoose__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("FiKB");
+/* harmony import */ var mongoose__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(mongoose__WEBPACK_IMPORTED_MODULE_0__);
+
+const connection = {};
+
+async function dbConnect() {
+  if (connection.isConnected) {
+    return;
+  }
+
+  const db = await mongoose__WEBPACK_IMPORTED_MODULE_0___default.a.connect("mongodb+srv://profiles:MongoPass@cluster0-f8o3q.gcp.mongodb.net/profiles?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  connection.isConnected = db.connections[0].readyState;
+  console.log(connection.isConnected);
+}
+
+/* harmony default export */ __webpack_exports__["a"] = (dbConnect);
+
+/***/ }),
+
+/***/ "j2p9":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _utils_dbConnect__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("RuLO");
+/* harmony import */ var _modals_book__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("/XmJ");
+/* harmony import */ var _modals_book__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_modals_book__WEBPACK_IMPORTED_MODULE_1__);
+
+
+Object(_utils_dbConnect__WEBPACK_IMPORTED_MODULE_0__[/* default */ "a"])();
+/* harmony default export */ __webpack_exports__["default"] = (async (req, res) => {
+  const {
+    method
+  } = req;
+  const myTitle = req.headers.title;
+  const myAuthor = req.headers.author;
+  console.log('title is ' + myTitle);
+  console.log('author is ' + myAuthor); // In case we have only the author as input, we will looking
+  // for all books with that author
+
+  if (myTitle == '' && myAuthor != '') {
+    console.log('No title');
+
+    switch (method) {
+      case 'GET':
+        try {
+          const books = await _modals_book__WEBPACK_IMPORTED_MODULE_1___default.a.find({
+            //Sort here
+            //'title': myTitle
+            'author': myAuthor
+          });
+          res.status(200).json({
+            success: true,
+            data: books
+          });
+        } catch (error) {
+          res.status(400).json({
+            success: false
+          });
+          console.log('Author not found');
+        }
+
+        break;
+
+      case 'POST':
+        try {
+          const book = await _modals_book__WEBPACK_IMPORTED_MODULE_1___default.a.create(req.body);
+          console.log('post');
+          console.log(req.body);
+          res.status(201).json({
+            success: true,
+            data: book
+          });
+        } catch (error) {
+          res.status(400).json({
+            success: false
+          });
+          console.log("couldn't post 1");
+        }
+
+        break;
+
+      default:
+        res.status(400).json({
+          success: false
+        });
+        break;
+    }
+  } //In case only the title is given as input,
+  // Look for all books with that title
+  else if (myAuthor == '' && myTitle != '') {
+      console.log('No author');
+
+      switch (method) {
+        case 'GET':
+          try {
+            const books = await _modals_book__WEBPACK_IMPORTED_MODULE_1___default.a.find({
+              //Sort here
+              'title': myTitle //'author': 'Robert Kiyosaki'
+
+            });
+            res.status(200).json({
+              success: true,
+              data: books
+            });
+          } catch (error) {
+            res.status(400).json({
+              success: false
+            });
+            console.log('Title not found');
+          }
+
+          break;
+
+        case 'POST':
+          try {
+            const book = await _modals_book__WEBPACK_IMPORTED_MODULE_1___default.a.create(req.body);
+            console.log('post');
+            console.log(req.body);
+            res.status(201).json({
+              success: true,
+              data: book
+            });
+          } catch (error) {
+            res.status(400).json({
+              success: false
+            });
+            console.log("couldn't post 2");
+          }
+
+          break;
+
+        default:
+          res.status(400).json({
+            success: false
+          });
+          break;
+      }
+    } // User input both the title and the author
+    // Return book matching both
+    else if (myTitle != '' && myAuthor != '') {
+        console.log('my title is: ' + myTitle);
+        console.log('my author is: ' + myAuthor);
+
+        switch (method) {
+          case 'GET':
+            try {
+              const books = await _modals_book__WEBPACK_IMPORTED_MODULE_1___default.a.find({
+                //Sort here
+                'title': myTitle,
+                'author': myAuthor
+              });
+              res.status(200).json({
+                success: true,
+                data: books
+              });
+            } catch (error) {
+              res.status(400).json({
+                success: false
+              });
+            }
+
+            break;
+
+          case 'POST':
+            try {
+              const book = await _modals_book__WEBPACK_IMPORTED_MODULE_1___default.a.create(req.body);
+              console.log(req.body);
+              res.status(201).json({
+                success: true,
+                data: book
+              });
+              console.log('succeeded, ' + myTitle + ' ' + myAuthor);
+            } catch (error) {
+              res.status(400).json({
+                success: false
+              });
+              console.log("couldn't post 3");
+              console.log('Failed: ' + myTitle + ' ' + myAuthor);
+            }
+
+            break;
+
+          default:
+            res.status(400).json({
+              success: false
+            });
+            break;
+        }
+      } // There is no given title and author
+      // We return all books available
+      else if (myTitle == '' && myAuthor == '') {
+          switch (method) {
+            case 'GET':
+              try {
+                const books = await _modals_book__WEBPACK_IMPORTED_MODULE_1___default.a.find({});
+                res.status(200).json({
+                  success: true,
+                  data: books
+                });
+              } catch (error) {
+                res.status(400).json({
+                  success: false
+                });
+              }
+
+              break;
+
+            case 'POST':
+              try {
+                const book = await _modals_book__WEBPACK_IMPORTED_MODULE_1___default.a.create(req.body);
+                console.log('post');
+                console.log(req.body);
+                res.status(201).json({
+                  success: true,
+                  data: book
+                });
+              } catch (error) {
+                res.status(400).json({
+                  success: false
+                });
+                console.log("couldn't post 4");
+              }
+
+              break;
+
+            default:
+              res.status(400).json({
+                success: false
+              });
+              break;
+          }
+        }
+  /*
+      switch(method){
+          case 'GET':
+              try {
+                  
+                  const books = await Book.find({
+                      //Sort here
+                      'title': myTitle
+                      //'author': 'Robert Kiyosaki'
+                  });
+                  
+  
+                  res.status(200).json({ success: true, data: books })
+              } catch (error) {
+                  res.status(400).json({ success: false });
+              }
+              
+              break;
+          case 'POST':
+              try {
+                  const book = await Book.create(req.body);
+  
+                  res.status(201).json({ success: true, data: book })
+              } catch (error) {
+                  res.status(400).json({ success: false });
+                  
+              }
+              break;
+          default:
+              res.status(400).json({ success: false });
+              break;
+      }
+      */
+
+});
+
+/***/ })
+
+/******/ });
--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     env: {
         MONGO_URI: "mongodb+srv://profiles:MongoPass@cluster0-f8o3q.gcp.mongodb.net/profiles?retryWrites=true&w=majority"
-    }
+    },
+    webpack(config, { dev }) {
+        // modify it!
+        return config
+      }
 }
+
