e47b0dad47972638184cb558805258c9f18ded21
Dhaneshen <dan.moonian.yahoo.com>
2020-07-17 19:57:27 -0600

    copied env to the _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,12 @@ import "bootstrap/dist/css/bootstrap.css";
 import '../styles/newBook.scss'
 import 'semantic-ui-css/semantic.min.css';
 
+module.exports = {
+  env: {
+      MONGO_URI: "mongodb+srv://profiles:MongoPass@cluster0-f8o3q.gcp.mongodb.net/profiles?retryWrites=true&w=majority"
+  }
+}
+
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
   }
