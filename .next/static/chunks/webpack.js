b165220559e03282a19fd408158e67e2128f3953
Dhaneshwaran Moonian <dan.moonian@gmail.com>
2020-09-07 19:05:20 -0600

    Fixing safari css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "08e1c66aaa45d907b6a0";
+/******/ 	var hotCurrentHash = "83c1160ae0dec4e286a6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/aboutus":["static\u002Fchunks\u002Fpages\u002Faboutus.js"],"/bookList":["static\u002Fchunks\u002Fpages\u002FbookList.js"],"/posts":["static\u002Fchunks\u002Fpages\u002Fposts.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/aboutus":["static\u002Fchunks\u002Fpages\u002Faboutus.js"],"/posts":["static\u002Fchunks\u002Fpages\u002Fposts.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
